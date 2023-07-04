4a5ae91ed7f0cd470005709067c4c4833a9b7d24
Daniel E. Antonsen <danieleantonsen@gmail.com>
2023-07-04 16:49:22 +0200

    updated file

--- src/main.c
+++ src/main.c
@@ -8,9 +8,33 @@
 Creating a simple bank manegment system
 */
 
+struct user 
+{
+    char name[25];
+    char pwd[25];
+};
 
-public int main()
+private void login(char name[], char password[])
 {
     
+}
+
+public int main()
+{   
+    char input;
+    printf("Hi! Welcome to Secret Bank! \n 1) Create new account.\n 2) Login.\n");
+    fgets(&input, sizeof(input), stdin);
+    switch (input)
+    {
+    case '1':
+        create_account();
+        break;
+    case '2':
+        login;
+        break;
+    default:
+        printf("Invalid input.\n Please enter either 1 or 2.");
+        break;
+    }
     return 0;
 }
