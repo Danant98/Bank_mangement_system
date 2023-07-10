ee86edfb3dea3e14d0f054e3fd90a66896b61043
Daniel E. Antonsen <danieleantonsen@gmail.com>
2023-07-10 18:28:30 +0200

    updated GUI

--- src/main.c
+++ src/main.c
@@ -17,23 +17,48 @@ struct user
 
 private void login(char name[], char password[])
 {
-    printf("");
+    printf("To be done!");
 }
 
 private void createAccount()
 {
-   printf(""); 
+   printf("To be done!"); 
 }
 
 private void getBalance(float balance)
-{
-    printf("\nYour balance is %f\n\n", balance);
+{   
+    System("cls");
+    int userInput;
+    
+    printf("\nBalance: %.3f\n\n", balance);
+
+    printf("\n\n 1) Back \n2) Exit");
+    printf("Enter: ");
+    scanf("%d", &userInput);
+
+    switch (userInput)
+    {
+    case 1:
+        break;
+    
+    case 2:
+        system("cls");
+        exit(0); 
+        break;
+    
+    default:
+        printf("\n\n Invalid command recived!\n. Please choose either 1 or 2");
+    }
 }
 
 private float depositBalance(float balance, float amount)
 {   
+    system("cls");
     balance += amount;
-    printf("\nYour new balance is %f\n\n", balance);
+    printf("\nBalance: %.3f\n\n", balance);
+    printf("");
+
+
     return balance;
 }
 
@@ -44,7 +69,7 @@ private float withdrawBalance(float balance, float amount)
         exit(0);
     } else {
         balance -= amount;
-        printf("\nYour new balance is %f\n\n", balance);
+        printf("\nYour new balance is %.3f\n\n", balance);
         return balance;
     }
 }
@@ -77,14 +102,16 @@ public int main()
 
    FILE *fptr;
 
+    /*
     fptr = fopen("balance.txt", "r");
     char balance;
     fgets(balance, 20, fptr);
     float balance;
-
+    */
+    char balance;
 
     system("cls");
-    printf("\n Hi! WELCOMME TO SECRET BANK\n\n");
+    printf("\n Hi! WELCOMME TO SECRET BANK\n\n"); 
 
     while (1) 
     {   
@@ -101,29 +128,37 @@ public int main()
             case 1:
                 getBalance(balance);
                 break;
+
             case 2: ;
                 float inputAmount;
                 printf("\nPlease spesify amount: ");
                 scanf("%f", &inputAmount);
                 balance = depositBalance(balance, inputAmount);
                 break;
+
             case 3: ;
                 float withdrawl;
                 printf("\nPlease spesify amount: ");
                 scanf("%f", &withdrawl);
                 balance = withdrawBalance(balance, withdrawl);
                 break;
+
             case 4:
                 system("cls");
                 exit(0);
                 break;
+
+            default:
+                printf("\n\n Invalid command recived!\n Please specify a number between 1-4.");
+                break;
         }
     }
     // Saving balance to text file
+    /*
     fptr = fopen("balance.txt", "w");
     fprintf(fptr, (char) balance); 
     fclose(fptr);
-
+    */
     return 0;
 }
  
